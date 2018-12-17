e408a6ea62d64c27bb5bb387124241bb651bffc3
Thomas Lau <thomas_0836@qq.com>
2018-12-17 16:04:28 +0800

    update version

--- config/plugin.config.js
+++ config/plugin.config.js
@@ -1,6 +1,5 @@
 // Change theme plugin
 
-import MergeLessPlugin from 'antd-pro-merge-less';
 import AntDesignThemePlugin from 'antd-theme-webpack-plugin';
 import path from 'path';
 
@@ -8,13 +7,6 @@ export default config => {
   // 将所有 less 合并为一个供 themePlugin使用
   const outFile = path.join(__dirname, '../.temp/u_webant.less');
   const stylesDir = path.join(__dirname, '../src/');
-
-  config.plugin('merge-less').use(MergeLessPlugin, [
-    {
-      stylesDir,
-      outFile,
-    },
-  ]);
 
   config.plugin('ant-design-theme').use(AntDesignThemePlugin, [
     {
