292fdff0fa1f893d8f30136bf667e2c781ff9bb3
Thomas Lau <thomas_0836@qq.com>
2018-12-18 13:24:51 +0800

    fix i18n Bug

--- src/components/SelectLang/index.js
+++ src/components/SelectLang/index.js
@@ -2,17 +2,31 @@ import React, { PureComponent } from 'react';
 import classNames from 'classnames';
 import intl from 'react-intl-universal';
 import styles from './index.less';
-
-const getLocale = () => localStorage.getItem('lang_type') || 'en-US';
+import { parse, stringify } from 'qs';
 
 export default class SelectLang extends PureComponent {
+
   render() {
-    const { className } = this.props;
-    const selectedLang = getLocale();
+    const { className, LS } = this.props;
+    const storage = LS || localStorage
+    
+    const selectedLang = storage.getItem('lang_type') || 'en-US';
     const canChangeLang = selectedLang === 'en-US' ? 'zh-CN' : 'en-US';
+
+    let url = window.location.href
+    console.log('url', url)
+    const urlArr = url.split('?')
+    if (urlArr.length === 1) {
+      url = `${url}?lan=${canChangeLang}`
+    } else {
+      const parms = parse(urlArr[1])
+      parms.lan = canChangeLang
+      url = `${urlArr[0]}?${stringify(parms)}`
+    }
+    
     return (
       <a
-        href={`?lan=${canChangeLang}`}
+        href={url}
         className={classNames(className, styles.menu)}
         title={intl.get('languages')}
       >
