8f80fadae02c95ec538f389c5b52bbe83f5b3e4c
Thomas Lau <thomas_0836@qq.com>
2018-12-19 15:23:00 +0800

    Out of the code

--- src/models/login.js
+++ src/models/login.js
@@ -3,94 +3,60 @@ import { setAuthority } from '@/utils/authority';
 import { reloadAuthorized } from '@/utils/Authorized';
 import { routerRedux } from 'dva/router';
 import { LS, getPageQuery } from '@/utils';
-import { stringify } from 'qs';
+import { login } from 'u_webant_base';
 
-export default {
-  namespace: 'login',
+const model = login({
+  logout, getSystemPath, setAuthority, reloadAuthorized, LS
+})
+model.effects = {
+  ...model.effects,
+  *login({ payload }, { put }) {
+    yield put({
+      type: 'changeLoginStatus',
+      payload,
+    });
+    LS.setItem('U_token', payload.token)
+    reloadAuthorized();
 
-  state: {
-    status: undefined,
-  },
-
-  reducers: {
-    changeLoginStatus(
-      state,
-      {
-        payload: { currentAuthority, status, type },
-      }
-    ) {
-      setAuthority(currentAuthority);
-      return { ...state, status, type };
-    },
-  },
-
-  effects: {
-    *login({ payload }, { put }) {
-      yield put({
-        type: 'changeLoginStatus',
-        payload,
-      });
-      LS.setItem('U_token', payload.token);
-      reloadAuthorized();
-
-      let { redirect } = payload;
-      if (!redirect) {
-        const params = getPageQuery();
-        const { redirect: url } = params;
-        redirect = url;
-      }
-
-      if (redirect) {
-        redirect = decodeURIComponent(redirect);
-        try {
-          const urlParams = new URL(window.location.href);
-          const redirectUrlParams = new URL(redirect);
-          // 判断是否同域
-          if (redirectUrlParams.origin === urlParams.origin) {
-            redirect = redirect.substr(urlParams.origin.length);
-            if (redirect.startsWith('/#')) {
-              redirect = redirect.substr(2);
+    let { redirect } = payload
+    if (!redirect){
+      const params = getPageQuery();
+      const { redirect: url } = params
+      redirect = url;
+    } 
+    
+    if (redirect) {
+      redirect = decodeURIComponent(redirect);
+      try {
+        const urlParams = new URL(window.location.href);
+        const redirectUrlParams = new URL(redirect);
+        // 判断是否同域
+        if (redirectUrlParams.origin === urlParams.origin) {
+          redirect = redirect.substr(urlParams.origin.length);
+          if (redirect.startsWith('/#')) {
+            redirect = redirect.substr(2);
+          }
+        } else {
+          if (redirect.indexOf('?') > 0) {
+            if (redirect.indexOf('utoken=') < 0) {
+              redirect = `${redirect}&utoken=${payload.token}`
             }
           } else {
-            if (redirect.indexOf('?') > 0) {
-              if (redirect.indexOf('utoken=') < 0) {
-                redirect = `${redirect}&utoken=${payload.token}`;
-              }
-            } else {
-              redirect = `${redirect}?utoken=${payload.token}`;
-            }
-            window.location.href = redirect;
-            return;
+            redirect = `${redirect}?utoken=${payload.token}`
           }
-        } catch (error) {
-          console.error('login model effects login', error); // eslint-disable-line
+          window.location.href = redirect;
+          return;
         }
+      } catch (error) {
+        console.error('login model effects login', error)
       }
-      yield put({
-        type: 'user/fetchCurrent',
-      });
-      yield put(routerRedux.replace(redirect || '/'));
-    },
+      
+    }
+    yield put({
+      type: 'user/fetchCurrent',
+    });
+    yield put(routerRedux.replace(redirect || '/'));
+  }
+}
 
-    *logout(_, { put, call }) {
-      yield call(logout);
-
-      yield put({
-        type: 'changeLoginStatus',
-        payload: {
-          status: false,
-          currentAuthority: 'guest',
-        },
-      });
-      yield put({
-        type: 'user/saveCurrentUser',
-        payload: {},
-      });
-      LS.removeItem('U_token');
-      reloadAuthorized();
-
-      const { data } = yield call(getSystemPath, 'loginweb');
-      window.location.href = `${data}/user/login?${stringify({ redirect: window.location.href })}`;
-    },
-  },
-};
+export default model;
--- src/models/user.js
+++ src/models/user.js
@@ -4,88 +4,35 @@ import { updateHeadimg, updateNickname } from '@/services/api';
 import { LS } from '@/utils';
 import { message } from 'antd';
 import intl from 'react-intl-universal';
+import { user } from 'u_webant_base';
 
-export default {
-  namespace: 'user',
-
-  state: {
-    currentUser: {},
-    UTOKEN: '',
+const model = user({
+  queryCurrent, reloadAuthorized, LS
+})
+model.effects = {
+  ...model.effects,
+  *updateHeadimg({ payload }, { call, put }) {
+    const {
+      data: { errSms, token },
+    } = yield call(updateHeadimg, payload);
+    if (token) {
+      LS.setItem('U_token', token);
+      yield put({ type: 'fetchCurrent' });
+    } else if (errSms) {
+      message.error(intl.get(errSms));
+    }
   },
-
-  effects: {
-    *fetchCurrent(_, { call, put }) {
-      const { data, token } = yield call(queryCurrent);
-      yield put({
-        type: 'saveCurrentUser',
-        payload: {
-          currentUser: data,
-          UTOKEN: token,
-        },
-      });
-      let currentAuthority = 'guest';
-      let status = false;
-      if (data && data.roles) {
-        currentAuthority = 'user';
-        if (data.roles) {
-          currentAuthority = data.roles;
-        }
-        status = data.isLock ? 'exception' : 'normal';
-      }
-      yield put({
-        type: 'login/changeLoginStatus',
-        payload: {
-          status,
-          currentAuthority,
-        },
-      });
-      reloadAuthorized();
-    },
-    *updateHeadimg({ payload }, { call, put }) {
-      const {
-        data: { errSms, token },
-      } = yield call(updateHeadimg, payload);
-      if (token) {
-        LS.setItem('U_token', token);
-        yield put({ type: 'fetchCurrent' });
-      } else if (errSms) {
-        message.error(intl.get(errSms));
-      }
-    },
-    *updateNickname({ payload }, { call, put }) {
-      const {
-        data: { errSms, token },
-      } = yield call(updateNickname, payload);
-      if (token) {
-        LS.setItem('U_token', token);
-        yield put({ type: 'fetchCurrent' });
-      } else if (errSms) {
-        message.error(intl.get(errSms));
-      }
-    },
+  *updateNickname({ payload }, { call, put }) {
+    const {
+      data: { errSms, token },
+    } = yield call(updateNickname, payload);
+    if (token) {
+      LS.setItem('U_token', token);
+      yield put({ type: 'fetchCurrent' });
+    } else if (errSms) {
+      message.error(intl.get(errSms));
+    }
   },
+}
 
-  reducers: {
-    saveCurrentUser(
-      state,
-      {
-        payload: { currentUser, UTOKEN },
-      }
-    ) {
-      return {
-        ...state,
-        currentUser: currentUser || {},
-        UTOKEN,
-      };
-    },
-    changeNotifyCount(state, action) {
-      return {
-        ...state,
-        currentUser: {
-          ...state.currentUser,
-          notifyCount: action.payload,
-        },
-      };
-    },
-  },
-};
+export default model;
