5bda5d0892b1f69e81cc9c044f683c7e3aa870ca
Thomas Lau <thomas_0836@qq.com>
2018-12-19 12:06:15 +0800

    分离了一些常用方法

--- src/components/SelectLang/index.js
+++ src/components/SelectLang/index.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import classNames from 'classnames';
 import intl from 'react-intl-universal';
 import styles from './index.less';
-import { parse, stringify } from 'qs';
+import { url } from 'util_react_web';
 
 export default class SelectLang extends PureComponent {
 
@@ -12,21 +12,12 @@ export default class SelectLang extends PureComponent {
     
     const selectedLang = storage.getItem('lang_type') || 'en-US';
     const canChangeLang = selectedLang === 'en-US' ? 'zh-CN' : 'en-US';
-
-    let url = window.location.href
-    console.log('url', url)
-    const urlArr = url.split('?')
-    if (urlArr.length === 1) {
-      url = `${url}?lan=${canChangeLang}`
-    } else {
-      const parms = parse(urlArr[1])
-      parms.lan = canChangeLang
-      url = `${urlArr[0]}?${stringify(parms)}`
-    }
+    const { addQuery } = url;
+    const uri = addQuery(window.location.href, 'lan', canChangeLang)
     
     return (
       <a
-        href={url}
+        href={uri}
         className={classNames(className, styles.menu)}
         title={intl.get('languages')}
       >
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,26 +1,11 @@
 import { parse } from 'qs';
 import config from './config';
 import request from './request';
-import LS from './storage';
+import LS from 'util_storage';
 import Delay from './delay';
+import { where, url } from 'util_react_web';
 
-const getPageQuery = () => parse(window.location.href.split('?')[1]);
-
-const where = () => {
-  const ua = window.navigator.userAgent.toLowerCase();
-
-  if (ua.match(/MicroMessenger/i)) {
-    return 'wechat';
-  }
-  if (
-    ua.match(
-      /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
-    )
-  ) {
-    return 'mobile';
-  }
-  return 'pc';
-};
+const { getPageQuery } = url;
 
 module.exports = {
   config,
